9d29cb2b68ae878bb5833c28c422bda22e675ca5
Nick Doiron <ndoiron@mapmeld.com>
2016-12-04 01:11:22 -0500

    use a trick to get all of the data

--- importer.js
+++ importer.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const geojson = require('esri-to-geojson');
 
 var ej = JSON.parse(fs.readFileSync('./page1.json', { encoding: 'utf-8' }));
+var ej2 = JSON.parse(fs.readFileSync('./page2.json', { encoding: 'utf-8' }));
+ej.features = ej.features.concat(ej2.features);
+
 var gj = geojson.fromEsri(ej);
 
-fs.writeFile('./page1.geojson', JSON.stringify(gj), () => {});
+fs.writeFile('./combined.geojson', JSON.stringify(gj), () => {});
