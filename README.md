27e7baf310ab163860546605de91d652e9dd6983
Nick Doiron <ndoiron@mapmeld.com>
2016-12-10 23:14:43 -0500

    load points on the map, include GitHub fetch polyfill

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pokemon Mars
+
+Pokemon Mars is a game based on Pokemon Go and nihilism.  You cannot win the game because
+you will never visit Mars or any other planet.
+
+Current status: displays your earth coordinates on top of MapBox Mars tiles and features
+
+## Resources used
+
+* MapBox Mars tiles: https://www.mapbox.com/blog/mars-maps/
+* LeafletJS: https://leafletjs.com
+* Fetch polyfill: https://github.com/github/fetch
+* NASA geographic features server (see import-data.js)
+
+## License
+
+Open source, MIT license
--- javascript/poke.js
+++ javascript/poke.js
@@ -1,6 +1,7 @@
 // create the Leaflet map
 var map = L.map('map')
   .setView([0, 0], 7);
+map.attributionControl.setPrefix('');
 
 // add the MapBox Mars Terrain tiles
 L.tileLayer('//{s}.tiles.mapbox.com/v4/matt.72ca085f/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ2VvcmVhY3RvciIsImEiOiJjaXU2NG14b3UwZmRhMm9wZ3hqMWtiaGpwIn0.b4vjSMyQlwcEK9dFR86tWA', {
@@ -8,4 +9,28 @@ L.tileLayer('//{s}.tiles.mapbox.com/v4/matt.72ca085f/{z}/{x}/{y}.png?access_toke
   maxZoom: 14
 }).addTo(map);
 
-map.attributionControl.setPrefix('');
+// 'geolocate' me
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    L.circleMarker([position.coords.latitude, position.coords.longitude], {
+      color: '#f00',
+      fillColor: '#f00',
+      strokeColor: '#f00'
+    }).addTo(map);
+    map.setView([position.coords.latitude, position.coords.longitude], 8);
+  });
+} else {
+  console.log('no geolocation');
+}
+
+fetch('data/combined.geojson')
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    for (var f = 0; f < json.features.length; f++) {
+      var feature = json.features[f];
+      var lat = feature.geometry.coordinates[1];
+      var lng = feature.geometry.coordinates[0];
+      L.circleMarker([lat, lng]).addTo(map).bindPopup(feature.properties.clean_name);
+    }
+  });
